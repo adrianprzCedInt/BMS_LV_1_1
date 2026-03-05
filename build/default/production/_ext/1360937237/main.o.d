6411033b172b3aa99ab8c21dd4aaf15708e203b9
adriperezlopez <149066553+adriperezlopez@users.noreply.github.com>
2026-03-05 16:33:40 +0100

    Tarea de prueba parpadeo led 3, funcionagit add .git add .git add .git add .git add .git add .!

---
+++ build/default/production/_ext/1360937237/main.o.d
@@ -0,0 +1,48 @@
+build/default/production/_ext/1360937237/main.o: ../src/main.c \
+ ../src/config/default/definitions.h \
+ ../src/config/default/peripheral/power/plib_power.h \
+ ../src/config/default/device.h \
+ ../src/config/default/toolchain_specifics.h \
+ ../src/config/default/peripheral/uart/plib_uart1.h \
+ ../src/config/default/peripheral/uart/plib_uart_common.h \
+ ../src/config/default/peripheral/can/plib_can1.h \
+ ../src/config/default/peripheral/can/plib_can_common.h \
+ ../src/config/default/peripheral/clk/plib_clk.h \
+ ../src/config/default/peripheral/gpio/plib_gpio.h \
+ ../src/config/default/peripheral/evic/plib_evic.h \
+ ../src/config/default/peripheral/wdt/plib_wdt.h \
+ ../src/config/default/peripheral/adc/plib_adc.h \
+ ../src/config/default/peripheral/spi/spi_master/plib_spi2_master.h \
+ ../src/config/default/peripheral/spi/spi_master/plib_spi_master_common.h \
+ ../src/config/default/peripheral/rcon/plib_rcon.h \
+ freeRTOS/include/FreeRTOS.h freeRTOS/include/FreeRTOSConfig.h \
+ freeRTOS/include/projdefs.h freeRTOS/include/portable.h \
+ freeRTOS/include/deprecated_definitions.h \
+ freeRTOS/portable/MPLAB/PIC32MX/portmacro.h \
+ freeRTOS/include/mpu_wrappers.h freeRTOS/include/task.h \
+ freeRTOS/include/list.h
+../src/config/default/definitions.h:
+../src/config/default/peripheral/power/plib_power.h:
+../src/config/default/device.h:
+../src/config/default/toolchain_specifics.h:
+../src/config/default/peripheral/uart/plib_uart1.h:
+../src/config/default/peripheral/uart/plib_uart_common.h:
+../src/config/default/peripheral/can/plib_can1.h:
+../src/config/default/peripheral/can/plib_can_common.h:
+../src/config/default/peripheral/clk/plib_clk.h:
+../src/config/default/peripheral/gpio/plib_gpio.h:
+../src/config/default/peripheral/evic/plib_evic.h:
+../src/config/default/peripheral/wdt/plib_wdt.h:
+../src/config/default/peripheral/adc/plib_adc.h:
+../src/config/default/peripheral/spi/spi_master/plib_spi2_master.h:
+../src/config/default/peripheral/spi/spi_master/plib_spi_master_common.h:
+../src/config/default/peripheral/rcon/plib_rcon.h:
+freeRTOS/include/FreeRTOS.h:
+freeRTOS/include/FreeRTOSConfig.h:
+freeRTOS/include/projdefs.h:
+freeRTOS/include/portable.h:
+freeRTOS/include/deprecated_definitions.h:
+freeRTOS/portable/MPLAB/PIC32MX/portmacro.h:
+freeRTOS/include/mpu_wrappers.h:
+freeRTOS/include/task.h:
+freeRTOS/include/list.h:
